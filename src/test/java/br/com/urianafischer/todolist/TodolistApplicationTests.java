2d73bcf7de2ba894f680f2e5d2d949751b6d9f09
urianamiranda <urianamiranda1@gmail.com>
2023-10-10 01:05:26 -0300

    Primeiro commit

---
+++ src/test/java/br/com/urianafischer/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.urianafischer.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
